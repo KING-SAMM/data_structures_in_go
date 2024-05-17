35c05ab693a778a53df4d4504dde5472f5efc1ca
KING-SAMM <kcsamm11@gmail.com>
2024-05-18 00:24:55 +0100

    Updated minHeap and ReadMe files

--- heaps/README.md
+++ heaps/README.md
@@ -37,8 +37,7 @@ Level 2 is filled from the right instead of left
 
 <img src="../assets/incomplete_tree2.png" alt="Incomplete tree 2" width="400">   
    
-3. Min-heap: Every parent's key (node value) must be smaller than its children nodes'. This ensures the parent key is the smallest within the heap.
-
+3. Min-heap: Every parent's key (node value) must be smaller than its children nodes'. This ensures the parent key is the smallest within the heap. 
 Max-heap: Every parent's key (node value) must be larger than its children nodes'. This ensures the parent key is the largest within the heap.
 
 
@@ -50,4 +49,34 @@ To do this we use the following formulae:
 - Calculate left-child Index: 2 * parentIndex + 1
 - Calculate Right-child Index: 2 * parentIndex + 2
 
-See the Min Heap [code here](./binary_heap/minHeap.go)
+See the Min-Heap [code here](./binary_heap/minHeap.go), and Max-Heap [here](./binary_heap/maxHeap.go)
+
+## Steps
+1. Define a struct for the minHeap
+```go
+type minHeap struct{
+	storage []int
+	size int
+}
+```
+2. Create a new minHeap
+```go
+func newHeap() *minHeap {
+	return &minHeap{
+		storage: make([]int, 0),
+		size: 0,
+	} 
+}
+```
+We should be able to:
+3. Get parent, left-child and right-child positions 
+4. Determine if the current node has parent, left-child or right-child, and return a boolean
+5. Get actual data from parent, left-child and right-child 
+6. Swap elements at given indices
+7. Insert data into the heap
+8. Perform heapify-up operation
+9. Remove the minimum data from the heap
+10. Heapify down
+11. Print the heap
+
+See the Min-Heap [code here](./binary_heap/minHeap.go), and Max-Heap [here](./binary_heap/maxHeap.go)
--- heaps/binary_heap/minHeap.go
+++ heaps/binary_heap/minHeap.go
@@ -3,7 +3,10 @@
 ------------------------------------------------------*/
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"errors"
+)
 
 // Define a struct for the minHeap
 type minHeap struct{
@@ -73,11 +76,11 @@ func (mh *minHeap) Insert(data int) {
 	mh.storage = append(mh.storage, data)
 	mh.size += 1
 	// Make sure the data ends up in the correct position
-	mh.heapifyUp()
+	mh.HeapifyUp()
 }
 
-// Perform heapify-up operation
-func (mh *minHeap) heapifyUp() {
+// Perform heapify-up operation ()
+func (mh *minHeap) HeapifyUp() {
 	// starting from index of our newly inserted node
 	currentIndex := mh.size - 1
 	// keep walking up the Tree, heapifying as we go
@@ -92,6 +95,53 @@ func (mh *minHeap) heapifyUp() {
 	}
 }
 
+// Remove the minimum data from the heap
+func (mh *minHeap) RemoveMin() int{
+	// Return an error if heap is empty
+	if mh.size == 0 {
+        fmt.Println(errors.New("Empty heap!"))
+    }
+	// Get the minimum element/value which should be at the root
+	data := mh.storage[0]
+	// Replace the element at the root with the last element in the heap
+	mh.storage[0] = mh.storage[mh.size - 1]
+	// Update the slice
+	mh.storage = mh.storage[: mh.size - 1]
+	// Reduce the heap size
+	mh.size -= 1
+	// Heapify down
+	mh.HeapifyDown()
+	// Return the removed data
+    return data
+}
+
+// Heapify down
+func (mh *minHeap) HeapifyDown() {
+	// Starting from the root node position
+	currentIndex := 0
+	// So long as left child exists
+	for mh.HasLeftChild(currentIndex) {
+		// grab its index
+		smallerChildIndex := mh.GetLeftChildIndex(currentIndex)
+		// Check if Right Child exists and if it's smaller than left child 
+		if mh.HasRightChild(currentIndex) && mh.RightChild(currentIndex) < mh.LeftChild(currentIndex) {
+			// If true reassign smallerChildIndex to RightChild's index
+			smallerChildIndex = mh.GetRightChildIndex(currentIndex)
+		}
+		// Check if current node is less than selected (smaller) child node
+		if mh.storage[currentIndex] < mh.storage[smallerChildIndex] {
+			// Break out of loop if true
+			break
+		} else {
+			// Else swap their positions
+			mh.Swap(currentIndex, smallerChildIndex)
+			// Move new position to node at smallerChildIndex
+			currentIndex = smallerChildIndex
+		}
+	}
+}
+
+
 // Print the heap
 func (mh *minHeap) Print() {
 	fmt.Println(mh.storage)
@@ -104,11 +154,18 @@ func main() {
 	min_heap.Insert(10)
 	min_heap.Insert(20)
 	min_heap.Insert(5)
-	min_heap.Insert(30)
-	min_heap.Insert(15)
-	min_heap.Insert(3)
+	min_heap.Insert(8)
 	min_heap.Insert(0)
+	min_heap.Insert(15)
+	min_heap.Insert(30)
+
 	// fmt.Println(min_heap)
+	min_heap.Print()
+	fmt.Println(min_heap.size)
+	fmt.Println("Min:", min_heap.RemoveMin())
+	min_heap.Print()
+	fmt.Println(min_heap.size)
+	fmt.Println("Min:", min_heap.RemoveMin())
 	min_heap.Print()
 	fmt.Println(min_heap.size)
  }
